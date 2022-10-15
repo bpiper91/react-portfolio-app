44e86c7613846cf78c2edcfb85f492d682f46e3f
bpiper91 <bpiper91@gmail.com>
2022-10-15 13:18:03 -0400

    added Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <li>
+                <a href="https://github.com/bpiper91">GitHub</a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/brett-piper-53b70830/">LinkedIn</a>
+            </li>
+            <li>
+                <a href="#">Third Platform</a>
+            </li>
+        </div>
+    );
+};
